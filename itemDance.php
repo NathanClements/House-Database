705f60f6a1a382c528c33f4b6b4de375f17cffde
NathanClements <nathan.ross.clements@hotmail.co.uk>
2014-10-10 11:38:14 +0100

    Create itemDance.php

---
+++ itemDance.php
@@ -0,0 +1,33 @@
+<?php
+include 'header.php';
+?>
+
+<?php
+$id=$_GET['id'];
+
+$SQLstring = "SELECT * FROM house WHERE id=$id";
+$DBConnect = server_connect($_SESSION['username'], $_SESSION['password'], "Unable to connect to server!");
+database_connect($DBConnect, "Unable to connect to database!");
+$QueryResult = query($DBConnect, $SQLstring, "Unable to execute query!");
+
+
+$Row = mysqli_fetch_assoc($QueryResult);
+$name = $Row['name'];
+$description = $Row['description'];
+$imageURL = $Row['imageURL'];
+$dir = 'http://localhost/House/';
+$filename = $dir . $imageURL;
+
+
+echo "<h3 style='display: block; text-align: center'>$name</h3>";
+echo "<p><img src='".$filename."' width='380' height='380' style='display: block; margin-left: auto; margin-right:auto'></p>";
+echo "<h3 style='display: block; text-align: center'>$description</h3>";
+?>
+
+<form action="ListDanceItem.php">
+	<input type="submit" value="Back">
+</form>
+
+<?php
+include 'footer.php';
+?>
